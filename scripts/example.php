bf64c9a394d51982d21e1b92f09400731f668e86
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2016-04-13 18:52:13 -0700

    Updated comments and typos

--- scripts/example.php
+++ scripts/example.php
@@ -1,7 +1,8 @@
 <?php
 
-$UtilityScripts["ScriptExample"] = function() {
+//Give the script a name that will be shown in the interface.
+$UtilityScripts["Example"] = function() {
 
-  CRM_Core_Session::setStatus("This is status output from the Example Script", "Example", "success");
+  CRM_Core_Session::setStatus("This is status output from the Example Script", "Example", "success", array("expires" => 0));
   echo "Here we go with some nice console output.";
 };
